398ee58a3556ba3a64b1262f6e76eda2ca1ca7e1
nyanfox <hahaithieu@gmail.com>
2025-09-29 19:23:27 +0700

    update LSU.sv LoadUnit.sv StoreUnit.sv

--- src/LSU.sv
+++ src/LSU.sv
@@ -24,14 +24,16 @@ module LSU (i_clk, i_reset, i_funct3, i_lsu_addr, i_st_data, i_lsu_wren, o_ld_da
  localparam SW = 3'b010;
  
  wire [31:0] ins_storeunit_o_data_1, ins_storeunit_o_data_2;
+ wire [31:0] ins_storeunit_o_mask_1, ins_storeunit_o_mask_2;
  wire [31:0] ins_cla_sum;
  wire ins_cla_cout;
  reg [31:0] data1_r, data2_r;
- reg [31:0] mem [0:MEM_SIZE];
+ 
+ (* ramstyle = "M10K" *) reg [31:0] mem [0:MEM_SIZE-1];
  
  CLA32bit ins_cla (.i_a(i_lsu_addr), .i_b(1), .i_cin(0), .o_sum(ins_cla_sum), .o_cout(ins_cla_cout));
  LoadUnit ins_loadunit (.i_data1(data1_r), .i_data2(data2_r), .i_funct3(i_funct3), .i_lsu_addr(i_lsu_addr[1:0]), .o_data(o_ld_data));
- StoreUnit ins_storeunit (.i_funct3(i_funct3), .i_lsu_addr(i_lsu_addr[1:0]), .i_mem1(data1_r), .i_mem2(data2_r), .i_data(i_st_data), .o_data_1(ins_storeunit_o_data_1), .o_data_2(ins_storeunit_o_data_2));
+ StoreUnit ins_storeunit (.i_clk(i_clk), .i_funct3(i_funct3), .i_lsu_addr(i_lsu_addr[1:0]), .i_data(i_st_data), .o_data_1(ins_storeunit_o_data_1), .o_data_2(ins_storeunit_o_data_2), .o_mask_1(ins_storeunit_o_mask_1), .o_mask_2(ins_storeunit_o_mask_2));
  
  always @(*) begin
      data1_r = mem[i_lsu_addr[11:0]];
@@ -40,8 +42,8 @@ module LSU (i_clk, i_reset, i_funct3, i_lsu_addr, i_st_data, i_lsu_wren, o_ld_da
  
 always @(posedge i_clk) begin
  if(i_lsu_wren == 1) begin
-     mem[i_lsu_addr[11:0]] <= ins_storeunit_o_data_1;
-     mem[ins_cla_sum[11:0]] <= ins_storeunit_o_data_2;
+     mem[i_lsu_addr[8:0]] <= (mem[i_lsu_addr[8:0]] & ins_storeunit_o_mask_1) | ins_storeunit_o_data_1;
+     mem[ins_cla_sum[8:0]] <= (mem[ins_cla_sum[8:0]] & ins_storeunit_o_mask_2) | ins_storeunit_o_data_2;
  end
 end
 
