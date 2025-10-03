1532d574583a7919fc881c86e5a2294adc454e68
nyanfox <hahaithieu@gmail.com>
2025-10-03 12:02:44 +0700

    update ProgramCounter.sv

--- src/ProgramCounter.sv
+++ src/ProgramCounter.sv
@@ -1,23 +1,17 @@
-module ProgramCounter (i_clk, i_reset, i_wb, i_pc_sel, i_pc_4, o_next);
+module ProgramCounter (i_clk, i_reset, i_pc_next, o_next);
 
- input i_clk, i_reset, i_pc_sel;
- input [31:0] i_wb;
- input [31:0] i_pc_4;
+ input i_clk, i_reset;
+ input [31:0] i_pc_next;
  output reg [31:0] o_next;
  
  always @(posedge i_clk) begin
-  if (i_reset) begin
-      o_next <= 32'h0000_0000;
+  if(i_reset==1) begin
+   o_next <= 31'h0000_0000;
   end
   else begin
-   if (i_pc_sel==1'b0) begin
-	    o_next <= i_pc_4 + 1;
-	end
-	else begin
-	    o_next <= i_wb;
-	end
+      o_next <= i_pc_next;
   end
+ 
  end
-
 
 endmodule
